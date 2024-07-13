6066116565fe9affbff59757d4c61b7df595289b
Hoc Pham <phamthaihoc008@gmail.com>
2024-07-13 22:24:26 +0700

    Add project files.

---
+++ NZWalk.API/Controllers/SinhVienController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalk.API.Controllers
+{
+    //https://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SinhVienController : ControllerBase
+    {
+        //https://localhost:portnumber/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[]
+            {
+                "John", "Jane", "June"
+            };
+            return Ok(studentNames);
+        }
+    }
+}
