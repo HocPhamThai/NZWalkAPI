18e5c5062a052df53632d0f1ecbebe4bbf646cc7
Hoc Pham <phamthaihoc008@gmail.com>
2024-07-14 15:23:47 +0700

    Create region Controller, Dbcontext,, connection to DB

---
+++ NZWalk.API/Controllers/RegionsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalk.API.Models.Domain;
+
+namespace NZWalk.API.Controllers
+{
+    /// <summary>
+    /// https://localhost:portnumber/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetRegions()
+        {
+            var regions = new List<Region>()
+            {
+                new Region()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Auckland Region",
+                    Code = "AKL",
+                    RegionImageUrl = "https://images.pexels.com/photos/18201083/pexels-photo-18201083/free-photo-of-sky-tower-in-auckland.jpeg?auto=compress&cs=tinysrgb&w=600"
+                },
+                new Region()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Wellington Region",
+                    Code = "WLG",
+                    RegionImageUrl = "https://images.pexels.com/photos/4350631/pexels-photo-4350631.jpeg?auto=compress&cs=tinysrgb&w=600"
+                }
+            };
+            return Ok(regions);
+        }
+    }
+}
